d7ea34f42efc7d4cedf47ef251481dceb77b7f41
Trần Công Tính <165116050+Hnit04@users.noreply.github.com>
2025-08-30 14:11:19 +0700

    commit bai09

---
+++ lab02/src/A/Bai09.ts
@@ -0,0 +1,15 @@
+const arr= [1,2,3,4,5,6,7,8,9,10];
+const filterEvenNumbers = new Promise<number[]>((resolve, reject) => {
+    setTimeout(() => {
+        try {
+            const evens = arr.filter(num => num % 2 === 0);
+            resolve(evens);
+        } catch (error) {
+            reject(error);
+        }
+    }, 1000);
+});
+
+filterEvenNumbers
+    .then(result => console.log("Even numbers:", result))
+    .catch(err => console.error("Error:", err));
