5a56ddb3e9b5db6e73e23a83c87bfdcfbc716b26
Trần Công Tính <165116050+Hnit04@users.noreply.github.com>
2025-08-30 13:13:09 +0700

    commit bai 1

---
+++ lab02/src/A/bai01.ts
@@ -0,0 +1,12 @@
+function helloAsync(): Promise<string> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("Hello Async");
+      }, 2000); 
+    });
+  }
+  
+  helloAsync().then((message) => {
+    console.log(message); 
+  });
+  
